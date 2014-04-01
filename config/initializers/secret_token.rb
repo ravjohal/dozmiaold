0f58dc8bb832c5e01b6274e0c616b1b47dd870e0
ravjohal <ravneetjohal@gmail.com>
2014-03-31 20:57:38 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dozmia::Application.config.secret_key_base = 'e89c84fbc22a694b56a503c5de3d1d0cb63a33de6b3613df91960f54b962f700d4834a1f879a59aa24aeba62aa7b5fe54815447267e21dcb69897ecb0e21639b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dozmia::Application.config.session_store :cookie_store, key: '_dozmia_session'
